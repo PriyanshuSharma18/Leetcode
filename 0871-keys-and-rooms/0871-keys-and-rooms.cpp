98448df391f84c6e159b5f4d759e5a7f93223f9d
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-08-22 03:35:40 +0530

    Time: 7 ms (65.46%), Space: 14.3 MB (27.63%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
+   void  dfs(int i, vector<vector<int>>& rooms, vector<bool>& vis){
+       vis[i]=true;
+       for(auto nbr:rooms[i]){
+           if(!vis[nbr]){
+                dfs(nbr, rooms, vis);
+           }
+       }
+   }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int size = rooms.size();
-        int goal = (size * (size - 1)) / 2;
-        int psize = 0;
-        int csize = 0;
-        int sum = 0;
-        std::unordered_map<int, int> accessibleRooms;
-
-        for (auto room : rooms[0]) {
-            accessibleRooms[room] = 1;
-            sum += room;
-            csize++;
+        int n = rooms.size();
+        vector<bool> vis(n,false);
+        dfs(0,rooms,vis);
+        for(auto i:vis){
+            if(!i)
+                return false;
         }
-        while (psize != csize) {
-            for (auto [key, value] : accessibleRooms) {
-                for (auto room : rooms[key]) {
-                    if (accessibleRooms[room] != 1) {
-                        csize++;
-                        accessibleRooms[room] = 1;
-                        sum += room;
-                    }
-                }
-            }
-            psize++;
-        }
-        return ((goal != sum) ? false : true);
+        return true;
     }
 };
