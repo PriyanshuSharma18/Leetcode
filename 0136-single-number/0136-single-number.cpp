18f86aeb2d7221ccaf50f81912792d1b68c813ac
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-08-31 11:26:44 +0530

    Time: 7 ms (96.21%), Space: 19.4 MB (77.82%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            bool flag=false;
-            for(int j=0;j<nums.size();j++){
-                if(i!=j && nums[i]==nums[j]){
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag==false){
-                return nums[i];
-            }
-        }
-        return -1;
+       int xorr = 0;
+       for(int  i = 0; i < nums.size(); i++){
+           xorr = xorr^nums[i];
+       }
+       return xorr;
     }
 };
