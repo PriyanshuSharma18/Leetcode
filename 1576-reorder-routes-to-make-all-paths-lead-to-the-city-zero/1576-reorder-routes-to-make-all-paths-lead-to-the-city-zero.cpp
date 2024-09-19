b537a8bdc55589bb2cdb91584ef08beb407628c7
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-09-19 21:40:18 +0530

    Time: 338 ms (49.92%), Space: 118.2 MB (53.74%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-int ans=0;
-    void dfs(unordered_map<int,vector<int>>&mp ,unordered_map<int,bool>&vis,int m){
-        vis[m]=true;
-        for(auto i:mp[m]){
-            if(i>=0){
-                if(!vis[i]){    
-                    dfs(mp,vis,i);
-                }
-            }
-            else{
-                i=abs(i);
-                if(!vis[i]){   
-                    ans++; 
-                    dfs(mp,vis,i);
+    int minReorder(int n, vector<vector<int>>& v) {
+        int ns=v.size();
+        vector<int>adj[n];
+        vector<int>vis(n,0);
+        set<pair<int,int>>s;
+        for(int i=0;i<ns;i++)
+        {
+            adj[v[i][0]].push_back(v[i][1]);
+            adj[v[i][1]].push_back(v[i][0]);
+            s.insert({v[i][0],v[i][1]});
+        }
+        queue<int>q;
+        q.push(0);
+        // vis[0]=1;
+        int count=0;
+        while(!q.empty())
+        {
+            auto p= q.front();
+            q.pop();
+            if(vis[p]==0)
+            {
+                vis[p]=1;
+                for(auto i:adj[p])
+                {
+                    q.push(i);
+                    if(vis[i]==0&&s.count({p,i}))
+                    count++;
                 }
             }
         }
-    }
-    // ans++
-    // void sol(unordered_map<int,vector<int>>&mp ,unordered_map<int,bool>&vis,int m){
-    //     for(auto i:mp[m]) {
-    //         if(!vis[i]){
-    //             dfs(mp,vis,i);
-    //             ans++;
-    //         }
-    //     }
-    // }
-    int minReorder(int n, vector<vector<int>>& c) {
-        int s=c.size();
-        unordered_map<int,vector<int>>mp;
-        unordered_map<int,bool>vis;
-        for(int i=0;i<s;i++){
-            mp[c[i][1]].push_back(c[i][0]);
-            mp[c[i][0]].push_back(-c[i][1]);
-        }
-        dfs(mp,vis,0);
-        if(ans==0)return ans;
-        return ans;
+        return count;  
     }
 };
