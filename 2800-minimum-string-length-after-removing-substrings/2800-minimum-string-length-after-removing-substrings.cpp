6e7fec103f142f3ffc0206bff0acf6d39ccdfad1
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-10-07 09:45:07 +0530

    Time: 0 ms (100%), Space: 9.5 MB (100%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int minLength(string s) {
-        stack<char> stack;
-
-        for (int i = 0; i < s.length(); i++) {
-            char cur_char = s[i];
-
-            if (stack.empty()) {
-                stack.push(cur_char);
-                continue;
-            }
-
-            if (cur_char == 'B' && stack.top() == 'A') {
-                stack.pop();
-            }
-            else if (cur_char == 'D' && stack.top() == 'C') {
-                stack.pop();
-            }
-            else {
-                stack.push(cur_char);
-            }
-        }
-
-        return stack.size();
+    static int minLength(string& s) {
+        const int n=s.size();
+        if (n<2) return n;
+        int sz=1;
+        for(int i=1; i<n; i++){
+            s[sz]=s[i];
+            if (sz>0 &&((s[sz-1]=='A' && s[i]=='B') ||
+                (s[sz-1]=='C' && s[i]=='D'))) sz--;
+            else sz++;
+        }  
+        return sz;  
     }
 };
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
