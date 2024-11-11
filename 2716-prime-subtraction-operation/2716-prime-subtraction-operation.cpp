a4ac04b3b902d607d559d81259eb32116f183ab8
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-11-11 21:36:02 +0530

    Time: 0 ms (100%), Space: 27.2 MB (81.15%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,33 +1,50 @@
-
 class Solution {
 public:
-    bool checkPrime(int x) {
-        if (x <= 1) return false;
-        for (int i = 2; i <= sqrt(x); i++) {
-            if (x % i == 0) {
+    bool primeSubOperation(vector<int>& nums) {
+        int maxElement = getMaxElement(nums);
+        
+        // Create Sieve of Eratosthenes array to identify prime numbers
+        vector<bool> sieve(maxElement + 1, true);
+        sieve[1] = false;
+        for (int i = 2; i <= sqrt(maxElement + 1); i++) {
+            if (sieve[i]) {
+                for (int j = i * i; j <= maxElement; j += i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+        
+        // Check if array can be made strictly increasing by subtracting prime numbers
+        int currValue = 1;
+        int i = 0;
+        while (i < nums.size()) {
+            int difference = nums[i] - currValue;
+            
+            // Return false if current number is already smaller than required value
+            if (difference < 0) {
                 return false;
+            }
+            
+            // Move to next number if difference is prime or zero
+            if (sieve[difference] == true || difference == 0) {
+                i++;
+                currValue++;
+            } else {
+                currValue++;
             }
         }
         return true;
     }
-
-    bool primeSubOperation(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            int bound = (i == 0) ? nums[i] : nums[i] - nums[i - 1];
-            
-            if (bound <= 0) return false;
-            
-            int largestPrime = 0;
-            for (int j = bound - 1; j >= 2; j--) {
-                if (checkPrime(j)) {
-                    largestPrime = j;
-                    break;
-                }
+    
+private:
+    // Helper method to find maximum element in array
+    int getMaxElement(vector<int>& nums) {
+        int max = -1;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
             }
-            
-            nums[i] -= largestPrime;
         }
-        
-        return true;
+        return max;
     }
 };
