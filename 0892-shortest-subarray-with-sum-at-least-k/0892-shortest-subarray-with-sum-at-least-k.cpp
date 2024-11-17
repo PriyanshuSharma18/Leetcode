4651172b357eb6323727f22276321859f225c8b5
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-11-17 17:50:26 +0530

    Time: 18 ms (98.01%), Space: 107.4 MB (96.57%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        int res = INT_MAX;
-        long long curSum = 0;
-        deque<pair<long long, int>> q;  // (prefix_sum, end_idx)
+        int n = nums.size();
+        vector<long long> sum(n + 1);
         
-        for (int r = 0; r < nums.size(); r++) {
-            curSum += nums[r];
-            
-            if (curSum >= k) {
-                res = min(res, r + 1);
-            }
-            
-            // Find the minimum valid window ending at r
-            while (!q.empty() && curSum - q.front().first >= k) {
-                auto [prefix, endIdx] = q.front();
-                q.pop_front();
-                res = min(res, r - endIdx);
-            }
-            
-            // Validate the monotonic deque
-            while (!q.empty() && q.back().first > curSum) {
-                q.pop_back();
-            }
-            q.push_back({curSum, r});
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + nums[i];
         }
         
-        return res == INT_MAX ? -1 : res;
+        vector<int> q(n + 1);
+        int l = 0, r = 0;
+        int minLength = n + 1;
+        
+        for (int i = 0; i < sum.size(); i++) {
+            while (r > l && sum[i] >= sum[q[l]] + k) {
+                minLength = min(minLength, i - q[l++]);
+            }
+            
+            while (r > l && sum[i] <= sum[q[r - 1]]) {
+                r--;
+            }
+            
+            q[r++] = i;
+        }
+        
+        return minLength <= n ? minLength : -1;
     }
 };
