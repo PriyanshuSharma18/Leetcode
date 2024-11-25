9279960b1f7cad0725b78b64a80b455b7c751a8d
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-11-25 09:53:46 +0530

    Time: 6 ms (67.07%), Space: 12.2 MB (63.55%) - LeetHub

--- 0787-sliding-puzzle/0787-sliding-puzzle.cpp
+++ 0787-sliding-puzzle/0787-sliding-puzzle.cpp
@@ -1,48 +1,49 @@
 class Solution {
 public:
     int slidingPuzzle(vector<vector<int>>& board) {
-        // Directions for possible swaps based on '0' position
-        vector<vector<int>> dir = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
-        string target = "123450";
-        set<string> vis; // Track visited configurations
-        queue<string> q;
-        string start = "";
-
-        // Convert 2D board to a single string
-        for (auto row : board) {
-            for (auto col : row) {
-                start += col + '0';
+        string s = "";
+        for (auto& r : board) {
+            for (int n : r) {
+                s += to_string(n);
             }
         }
-
-        q.push(start);
-        vis.insert(start);
-        int step = 0;
-
-        // Perform BFS
+        
+        string g = "123450";
+        
+        vector<vector<int>> n = {
+            {1, 3},
+            {0, 2, 4},
+            {1, 5},
+            {0, 4},
+            {1, 3, 5},
+            {2, 4}
+        };
+        
+        queue<pair<string, int>> q;
+        unordered_set<string> v;
+        
+        q.push({s, 0});
+        v.insert(s);
+        
         while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                string current = q.front();
-                q.pop();
-
-                // Check if target is reached
-                if (current == target) return step;
-
-                int zero = current.find('0'); // Find position of '0'
-
-                // Generate next moves
-                for (auto move : dir[zero]) {
-                    string next = current;
-                    swap(next[move], next[zero]);
-                    if (!vis.count(next)) { // Add unvisited states to the queue
-                        vis.insert(next);
-                        q.push(next);
-                    }
+            auto [c, m] = q.front();
+            q.pop();
+            
+            if (c == g) return m;
+            
+            int z = c.find('0');
+            
+            for (int x : n[z]) {
+                string t = c;
+                swap(t[z], t[x]);
+                
+                if (v.find(t) == v.end()) {
+                    q.push({t, m + 1});
+                    v.insert(t);
                 }
             }
-            step++;
         }
-        return -1; // Return -1 if target is unreachable
+        
+        return -1;
     }
 };
