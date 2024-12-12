ecdbab2d9f1256974dd659828417e5b0a2e9f81e
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-12-12 17:34:01 +0530

    Time: 2 ms (61.64%), Space: 13.5 MB (7.74%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,16 +1,22 @@
+#include <queue>
+#include <vector>
+#include <cmath>
+
 class Solution {
 public:
-    long long pickGifts(vector<int>& gifts, int k) {
-        while (k--) {
-            sort(gifts.rbegin(), gifts.rend());
-            int pile = gifts[0];
-            gifts.erase(gifts.begin());
-            gifts.push_back(sqrt(pile));
+    long long pickGifts(std::vector<int>& gifts, int k) {
+        std::priority_queue<int> pq(gifts.begin(), gifts.end());
+        for (int i = 0; i < k; ++i) {
+            int gift = pq.top();
+            pq.pop();
+            int left = static_cast<int>(std::floor(std::sqrt(gift)));
+            pq.push(left);
         }
-        long long ans = 0;
-        for (auto u : gifts) {
-            ans += u;
+        long long sum = 0;
+        while (!pq.empty()) {
+            sum += pq.top();
+            pq.pop();
         }
-        return ans;
+        return sum;
     }
 };
