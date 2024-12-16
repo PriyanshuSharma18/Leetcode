5f47ad957c2831637c035b8cdb8b6bd1fc80f1d3
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-12-16 11:11:06 +0530

    Time: 0 ms (100%), Space: 27.7 MB (29.75%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        while(k--) *min_element(begin(nums), end(nums)) *= multiplier;
+        vector<pair<int, int>> heap;
+        for (int i = 0; i < nums.size(); i++) {
+            heap.push_back({nums[i], i});
+        }
+
+        make_heap(heap.begin(), heap.end(), greater<>());
+
+        while (k--) {
+            pop_heap(heap.begin(), heap.end(), greater<>());
+            auto [value, i] = heap.back();
+            heap.pop_back();
+
+            nums[i] *= multiplier;
+            heap.push_back({nums[i], i});
+            push_heap(heap.begin(), heap.end(), greater<>());
+        }
+
         return nums;
     }
 };
