30abcaf7bf761924655e2c5de9f89f0e70037918
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-12-20 08:05:03 +0530

    Time: 3 ms (50.72%), Space: 83.2 MB (26.01%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -1,33 +1,44 @@
-
-class Solution {
+//Please upvote if it helps :)
+class Solution 
+{
 public:
-    TreeNode* reverseOddLevels(TreeNode* root) {
+    TreeNode* reverseOddLevels(TreeNode* root) 
+    {
+        if(!root) return root;
+        
         queue<TreeNode*> q;
+        vector<int> values;
         q.push(root);
-        bool flag = false;  // Flag to track odd levels
-        while (!q.empty()) {
-            int sz = (int)q.size();  // Number of nodes at the current level
-            vector<TreeNode*> v;
-            // Traverse the current level
-            for (int i = 0; i < sz; i++) {
-                if (q.front() == NULL) {
-                    q.pop();
-                    continue;
+        int level=0;
+        
+        while(!q.empty())
+        {
+            int sz = q.size();
+            vector<int> temp;
+            for(int i=0; i<sz; i++)
+            {
+                TreeNode* node = q.front(); q.pop();
+                
+                if(level%2)
+                    node->val = values[sz-i-1];
+                
+                if(node->left)
+                {
+                    q.push(node->left);
+                    temp.push_back(node->left->val);
                 }
-                v.push_back(q.front());  // Collect nodes of this level
-                q.push(q.front()->left);  // Add left child
-                q.push(q.front()->right); // Add right child
-                q.pop();  // Remove processed node
-            }
-            // Reverse the nodes at odd levels
-            if (flag) {
-                int L = 0, R = (int)v.size() - 1;
-                while (L <= R) {
-                    swap(v[L++]->val, v[R--]->val);  // Swap values
+                if(node->right) 
+                {
+                    q.push(node->right);
+                    temp.push_back(node->right->val);
                 }
+                
             }
-            flag = (!flag);  // Toggle flag for the next level
+            values = temp;
+            level++;
         }
         return root;
+            
+        
     }
 };
