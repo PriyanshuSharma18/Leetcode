965566c28392d42db583ff784ab154c37acf698c
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2024-12-20 08:06:55 +0530

    Time: 0 ms (100%), Space: 76.9 MB (78.28%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -1,33 +1,23 @@
-
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        bool flag = false;  // Flag to track odd levels
-        while (!q.empty()) {
-            int sz = (int)q.size();  // Number of nodes at the current level
-            vector<TreeNode*> v;
-            // Traverse the current level
-            for (int i = 0; i < sz; i++) {
-                if (q.front() == NULL) {
-                    q.pop();
-                    continue;
-                }
-                v.push_back(q.front());  // Collect nodes of this level
-                q.push(q.front()->left);  // Add left child
-                q.push(q.front()->right); // Add right child
-                q.pop();  // Remove processed node
-            }
-            // Reverse the nodes at odd levels
-            if (flag) {
-                int L = 0, R = (int)v.size() - 1;
-                while (L <= R) {
-                    swap(v[L++]->val, v[R--]->val);  // Swap values
-                }
-            }
-            flag = (!flag);  // Toggle flag for the next level
-        }
+        traverseDFS(root->left, root->right, 0);
         return root;
+    }
+
+private:
+    void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
+        if (leftChild == nullptr || rightChild == nullptr) {
+            return;
+        }
+        // If the current level is odd, swap the values of the children.
+        if (level % 2 == 0) {
+            int temp = leftChild->val;
+            leftChild->val = rightChild->val;
+            rightChild->val = temp;
+        }
+
+        traverseDFS(leftChild->left, rightChild->right, level + 1);
+        traverseDFS(leftChild->right, rightChild->left, level + 1);
     }
 };
