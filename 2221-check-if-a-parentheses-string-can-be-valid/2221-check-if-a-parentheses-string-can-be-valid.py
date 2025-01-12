c34e50c129c3ad1b26d688d5523f4d2477364e8b
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-01-12 23:36:49 +0530

    Time: 107 ms (52.74%), Space: 18.8 MB (38.33%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.py
@@ -1,27 +1,30 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        n=len(s)
-        if n&1==1: return False
-        
-        bMin, bMax=0, 0  # Initialize balance
-        
-        for i, c in enumerate(s):
-            op=c== '('
-            wild=locked[i]=='0'
-            
-            # Update balances
-            if (not op) or wild: bMin-=1  
-            else: bMin+=1  
-            
-            if op or wild: bMax+=1  
-            else: bMax-=1  
-            
-            if bMax < 0: return False
-            
-            bMin = max(bMin, 0)
-        
-        # Check if the parentheses can be 
-        return bMin==0
+        n = len(s)
+        if n % 2 != 0:
+            return False
 
-        
-        
+        open_count = close_count = unlocked1 = unlocked2 = 0
+        for i in range(n):
+            # Left-to-right
+            if locked[i] == '0':
+                unlocked1 += 1
+            elif s[i] == '(':
+                open_count += 1
+            elif s[i] == ')':
+                open_count -= 1
+            if open_count + unlocked1 < 0:
+                return False
+
+            # Right-to-left
+            j = n - i - 1
+            if locked[j] == '0':
+                unlocked2 += 1
+            elif s[j] == ')':
+                close_count += 1
+            elif s[j] == '(':
+                close_count -= 1
+            if close_count + unlocked2 < 0:
+                return False
+
+        return True
