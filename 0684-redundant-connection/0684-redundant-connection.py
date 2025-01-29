663e84c4fd5e3de2c3f3f0ec00ca998144cfcb58
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-01-30 00:31:29 +0530

    Time: 0 ms (100%), Space: 18.5 MB (22.29%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,17 +1,35 @@
-class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        root = list(range(len(edges) + 1)) # each node is root of own subtree
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        n = len(edges)
+        parent = [i for i in range(n + 1)]  # Initialize each node as its own parent
+        rank = [0] * (n + 1)  # Rank for union by rank
 
-        def find_root(node):
-            if root[node] != node:
-                root[node] = find_root(root[node]) # set root for node as root of subtree
-            return root[node] # found root of subtree
+        def find(i):
+            if parent[i] != i:
+                parent[i] = find(parent[i])  # Path compression
+            return parent[i]
 
-        for node1, node2 in edges: 
-            root1, root2 = find_root(node1), find_root(node2)
+        def join(u, v):
+            rootU = find(u)
+            rootV = find(v)
 
-            if root1 == root2:
-                return [node1, node2] # both nodes are already in same subtree
- 
-            root[root2] = root1 # found new connection between nodes / subtrees
-            # root[root1] = root2 also works            
+            if rootU != rootV:
+                # Union by rank
+                if rank[rootU] > rank[rootV]:
+                    parent[rootV] = rootU
+                elif rank[rootU] < rank[rootV]:
+                    parent[rootU] = rootV
+                else:
+                    parent[rootV] = rootU
+                    rank[rootU] += 1
+
+        for u, v in edges:
+            if find(u) == find(v):
+                return [u, v]  # Cycle detected
+            join(u, v)  # Merge sets
+
+        return []  # Unreachable for valid inputs
