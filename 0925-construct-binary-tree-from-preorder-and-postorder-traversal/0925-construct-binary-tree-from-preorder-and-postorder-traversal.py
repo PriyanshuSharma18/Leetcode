ff825ada3ecf5be8c165a2dfd4faaeda1fe43067
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-02-24 04:31:48 +0530

    Time: 3 ms (49.92%), Space: 17.8 MB (70.85%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -1,23 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def constructFromPrePost(self, pre: List[int], post: List[int]) -> Optional[TreeNode]:
-        index = [0]  # Mutable index to track preorder position
-        return self.construct(pre, post, index, 0, len(pre) - 1)
-
-    def construct(self, pre, post, index, l, h):
-        if index[0] >= len(pre) or l > h:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not postorder:
             return None
-
-        root = TreeNode(pre[index[0]])
-        index[0] += 1
-        if l == h:
-            return root
-
-        i = l
-        while i <= h and post[i] != pre[index[0]]:
-            i += 1
-
-        if i <= h:
-            root.left = self.construct(pre, post, index, l, i)
-            root.right = self.construct(pre, post, index, i + 1, h - 1)
-
-        return root
+        node = TreeNode(preorder[0])
+        idx = postorder.index(preorder[1]) if len(preorder)>1 else 0
+        l = idx+1
+        node.left = self.constructFromPrePost(preorder[1:l+1],postorder[:l])
+        node.right = self.constructFromPrePost(preorder[l+1:],postorder[l:-1])
+        return node
