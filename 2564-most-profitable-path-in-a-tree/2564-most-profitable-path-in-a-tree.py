58a27b52502fd9534f6c33220e99ff542d63f643
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-02-25 01:15:36 +0530

    Time: 210 ms (76.08%), Space: 58.3 MB (79.07%) - LeetHub

--- 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.py
+++ 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.py
@@ -1,32 +1,39 @@
 class Solution:
     def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
-        graph = {i: [] for i in range(len(amount))}
+        n = len(amount)
+
+        graph = [[] for _ in range(n)] # make graph with connected nodes for each node
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        
-        bobPath = [-1] * len(amount)
-        path = []
 
-        def fillBobPath(node, parent):
-            if node == 0:
+        bob_time = [n] * n # [node] = time, time that bob reached node in path
+        def find_bob_path(node, prev, time): 
+            if node == 0: 
+                bob_time[node] = time # 0 is last in path
                 return True
-            for neighbor in graph[node]:
-                if neighbor != parent:
-                    path.append(node)
-                    if fillBobPath(neighbor, node):
-                        return True
-                    path.pop()
+            for nxt in graph[node]:
+                if nxt != prev and find_bob_path(nxt, node, time + 1): # avoid loops and check connected nodes
+                    bob_time[node] = time # path lead to 0, set time for current node
+                    return True
+            return False # path didn't lead to 0
+        find_bob_path(bob, -1, 0) # set values for bob_time
 
-        fillBobPath(bob, -1)
-        for i, node in enumerate(path):
-            bobPath[node] = i
-        
-        def getAliceMaxScore(node, parent, currScore, timestamp):
-            if bobPath[node] == -1 or bobPath[node] > timestamp:
-                currScore += amount[node]
-            elif bobPath[node] == timestamp:
-                currScore += amount[node] // 2
-            return currScore if len(graph[node]) == 1 and node != 0 else max(getAliceMaxScore(neighbor, node, currScore, timestamp + 1) for neighbor in graph[node] if neighbor != parent)
+        max_income = float('-inf')
+        def find_alice_path(node, prev, time, income):
+            if time < bob_time[node]:
+                income += amount[node] # alice reached gate node first
+            elif time == bob_time[node]:
+                income += amount[node] // 2 # alice and bob arrived at same time
 
-        return getAliceMaxScore(0, -1, 0, 0)
+            if node != 0 and len(graph[node]) == 1: # check if at leaf, node != 0 to pass starting node
+                nonlocal max_income
+                max_income = max(max_income, income) # set new max income
+                return # arrived at leaf, path over
+
+            for nxt in graph[node]: # exlpore connected nodes
+                if nxt != prev: # avoid looping
+                    find_alice_path(nxt, node, time + 1, income)
+
+        find_alice_path(0, -1, 0, 0) # find alice's max income
+        return max_income
