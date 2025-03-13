d36a96a57409bdd769320bd9d1b07fdce63d0425
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-03-14 03:44:56 +0530

    Time: 141 ms (91.87%), Space: 63 MB (94.58%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -1,37 +1,19 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        n = len(nums)
+        n, m=len(nums), len(queries)
+        freq=[0]*(n+1)
+        op, k=0, 0
+        for i in range(n):
+            while op<nums[i]-freq[i]:
+                if k>=m: 
+                    return -1
+                l, r, v=queries[k]
+                if r<i:
+                    k+=1 
+                    continue
+                freq[max(l, i)]+=v
+                freq[r+1]-=v
+                k+=1
+            op+=freq[i]
+        return k 
         
-        def can_make_zero_array(k):
-            diff = [0] * (n + 1)
-            
-            for i in range(k):
-                left, right, val = queries[i]
-                diff[left] += val
-                diff[right + 1] -= val
-            
-            curr_val = 0
-            for i in range(n):
-                curr_val += diff[i]
-                if curr_val < nums[i]:
-                    return False
-            
-            return True
-        
-        if all(x == 0 for x in nums):
-            return 0
-        
-        left, right = 1, len(queries)
-        
-        if not can_make_zero_array(right):
-            return -1
-        
-        while left < right:
-            mid = left + (right - left) // 2
-            
-            if can_make_zero_array(mid):
-                right = mid
-            else:
-                left = mid + 1
-        
-        return left
