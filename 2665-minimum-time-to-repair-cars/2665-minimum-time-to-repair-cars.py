47ced11ce35d8c08543ee6b772295d9be994aeab
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-03-17 02:20:14 +0530

    Time: 843 ms (43.9%), Space: 21.9 MB (84.55%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -1,3 +1,18 @@
 class Solution:
-    def repairCars(self, r: List[int], c: int) -> int:
-        return bisect_left(range(1<<47),c,key=lambda m:sum(isqrt(m//x)for x in r))
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        def can_repaired(time):
+            cnt = 0
+            for r in ranks:
+                cnt += int(sqrt(time / r))
+            return cnt >= cars
+        
+        l, r = 1, min(ranks) * cars * cars
+
+        while l <= r:
+            m = (l + r) // 2
+            if can_repaired(m):
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return l
