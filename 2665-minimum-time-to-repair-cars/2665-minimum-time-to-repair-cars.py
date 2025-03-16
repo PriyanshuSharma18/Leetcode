6630a972b1ffae6154eec9eb4921f79259cb4a83
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-03-17 02:19:24 +0530

    Time: 811 ms (45.85%), Space: 21.9 MB (49.43%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -1,20 +1,3 @@
-import math
-
-class Solution(object):
-    def time_is_suff(self, ranks, cars, min_given):
-        cars_done = 0
-        for r in ranks:
-            c2 = min_given // r
-            c = int(math.sqrt(c2)) 
-            cars_done += c
-        return cars_done >= cars
-
-    def repairCars(self, ranks, cars):
-        l, r = 1, int(1e14)
-        while l < r:
-            mid = (l + r) // 2
-            if self.time_is_suff(ranks, cars, mid):
-                r = mid
-            else:
-                l = mid + 1
-        return l
+class Solution:
+    def repairCars(self, r: List[int], c: int) -> int:
+        return bisect_left(range(1<<47),c,key=lambda m:sum(isqrt(m//x)for x in r))
