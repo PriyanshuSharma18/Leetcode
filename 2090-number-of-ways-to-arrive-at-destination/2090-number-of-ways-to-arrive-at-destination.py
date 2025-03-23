db64519c20fa100b0a1fe89dc20aaf5fc07f5ec3
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-03-24 00:52:40 +0530

    Time: 12 ms (94.48%), Space: 25 MB (86.77%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
@@ -1,33 +1,30 @@
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        graph = [[] for _ in range(n)]
-        for u, v, time in roads:
-            graph[u].append((v, time))
-            graph[v].append((u, time))
+        MOD = 10**9 + 7
+        adj = [[] for _ in range(n)]
+        
+        for u, v, t in roads:
+            adj[u].append((v, t))
+            adj[v].append((u, t))
             
+        heap = [(0, 0)]
         dist = [float('inf')] * n
         ways = [0] * n
-        
         dist[0] = 0
         ways[0] = 1
         
-        pq = [(0, 0)]
-        
-        MOD = 10**9 + 7
-        
-        # Dijkstra's algorithm
-        while pq:
-            d, node = heapq.heappop(pq)
-            
-            if d > dist[node]:
+        while heap:
+            time, u = heapq.heappop(heap)
+            if time > dist[u]:
                 continue
                 
-            for neighbor, time in graph[node]:
-                if dist[node] + time < dist[neighbor]:
-                    dist[neighbor] = dist[node] + time
-                    ways[neighbor] = ways[node]
-                    heapq.heappush(pq, (dist[neighbor], neighbor))
-                elif dist[node] + time == dist[neighbor]:
-                    ways[neighbor] = (ways[neighbor] + ways[node]) % MOD
-        
-        return ways[n-1] 
+            for v, t in adj[u]:
+                new_time = time + t
+                if new_time < dist[v]:
+                    dist[v] = new_time
+                    ways[v] = ways[u]
+                    heapq.heappush(heap, (new_time, v))
+                elif new_time == dist[v]:
+                    ways[v] = (ways[v] + ways[u]) % MOD
+                    
+        return ways[-1]
