1bb679575584816ac70226706f5422ac0c3c77c7
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-03-29 02:04:48 +0530

    Time: 557 ms (71.14%), Space: 32.7 MB (63.09%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
@@ -1,32 +1,27 @@
-from queue import PriorityQueue
-
 class Solution:
-    def maxPoints(self, grid, queries):
-        rows, cols = len(grid), len(grid[0])
-        DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+
+        findBest = lambda x: (bisect_left(arr, x))
+
+        m, n, prev = len(grid),len(grid[0]), 0
+        heap, arr = [(grid[0][0], (0, 0))], []
+        dir = ((1,0), (0,1), (-1,0), (0,-1))
+
+        grid[0][0] = None
         
-        sorted_queries = sorted([(val, idx) for idx, val in enumerate(queries)])
-        result = [0] * len(queries)
-        
-        heap = PriorityQueue()
-        visited = [[False] * cols for _ in range(rows)]
-        
-        heap.put((grid[0][0], 0, 0))
-        visited[0][0] = True
-        points = 0
-        
-        for query_val, query_idx in sorted_queries:
-            while not heap.empty() and heap.queue[0][0] < query_val:
-                _, row, col = heap.get()
-                points += 1
-                
-                for dr, dc in DIRECTIONS:
-                    nr, nc = row + dr, col + dc
-                    if (0 <= nr < rows and 0 <= nc < cols and 
-                        not visited[nr][nc]):
-                        heap.put((grid[nr][nc], nr, nc))
-                        visited[nr][nc] = True
-            
-            result[query_idx] = points
-        
-        return result
+        while heap:
+            cellVal, (row, col) = heappop(heap)
+            arr.append(cellVal)
+
+            for dr, dc in dir:
+                r, c = row + dr, col + dc
+
+                if 0 <= r < m and 0 <= c < n and grid[r][c]:
+                    
+                    heappush(heap, (grid[r][c], (r, c)))
+                    grid[r][c] = None
+
+        for i in range(m*n):
+            prev = arr[i] = max(arr[i],prev)
+
+        return [findBest(q) for q in queries]
