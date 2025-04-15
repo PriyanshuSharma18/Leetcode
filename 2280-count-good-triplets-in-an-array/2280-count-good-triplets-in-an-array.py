e30e5cab6f59a52573025361dc1c3021856611a2
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-04-15 09:55:52 +0530

    Time: 655 ms (78.3%), Space: 34.3 MB (98.11%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
@@ -1,15 +1,32 @@
+class Fenwick:
+    def __init__(self, n):
+        self.tree = [0] * (n + 1)
+    def update(self, k, x):
+        k += 1
+        while k < len(self.tree):
+            self.tree[k] += x
+            k += k & -k
+    def query(self, k):
+        total = 0
+        k += 1
+        while k > 0:
+            total += self.tree[k]
+            k -= k & -k
+        return total
+
 class Solution(object):
     def goodTriplets(self, nums1, nums2):
-        mpp = {}
-        for i, v in enumerate(nums1):
-            mpp[v] = i
-        n = len(nums2)
+        n = len(nums1)
         total = 0
-        st = []
-        for x in nums2:
-            idx = mpp[x]
-            left = bisect.bisect_left(st, idx)
-            right = (n - 1 - idx) - (len(st) - left)
+        mpp = [0] * n
+        for i in range(n):
+            mpp[nums1[i]] = i
+        fw = Fenwick(n)
+        for i in nums2:
+            idx = mpp[i]
+            left = fw.query(idx)
+            total_inner = fw.query(n - 1)
+            right = (n - 1 - idx) - (total_inner - left)
             total += left * right
-            bisect.insort(st, idx)
+            fw.update(idx, 1)
         return total
