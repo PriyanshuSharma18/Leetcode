5273e00bda65ec5fc0c3787efffd5ab0671a6be2
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-05-10 01:38:49 +0530

    Time: 1175 ms (92.77%), Space: 99.5 MB (37.35%) - LeetHub

--- 3637-count-number-of-balanced-permutations/3637-count-number-of-balanced-permutations.py
+++ 3637-count-number-of-balanced-permutations/3637-count-number-of-balanced-permutations.py
@@ -1,27 +1,17 @@
-class Solution(object):
-    def countBalancedPermutations(self, num):
-        mod = 10**9+7
-        n = len(num)
+class Solution:
+    def countBalancedPermutations(self, num: str) -> int:
+        count = Counter(int(c) for c in num)
         total = sum(int(c) for c in num)
-        if total % 2: return 0
-        fact = [1]*(n+1)
-        inv = [1]*(n+1)
-        invFact = [1]*(n+1)
-        for i in range(1,n+1): fact[i] = fact[i-1]*i % mod
-        for i in range(2,n+1): inv[i] = mod - (mod//i)*inv[mod%i] % mod
-        for i in range(1,n+1): invFact[i] = invFact[i-1]*inv[i] % mod
-        halfSum = total//2
-        halfLen = n//2
-        dp = [[0]*(halfLen+1) for _ in range(halfSum+1)]
-        dp[0][0] = 1
-        digits = [0]*10
-        for c in num:
-            d = int(c)
-            digits[d] += 1
-            for i in range(halfSum, d-1, -1):
-                for j in range(halfLen, 0, -1):
-                    dp[i][j] = (dp[i][j] + dp[i-d][j-1]) % mod
-        res = dp[halfSum][halfLen]
-        res = res * fact[halfLen] % mod * fact[n-halfLen] % mod
-        for cnt in digits: res = res * invFact[cnt] % mod
-        return res
+        MOD = 10**9 + 7
+        n = len(num)
+        @cache
+        def DFS(i, odd, even, balance):
+            if odd == 0 and even == 0 and balance == 0:
+                return 1
+            if i < 0 or odd < 0 or even < 0 or balance < 0:
+                return 0
+            res = 0
+            for j in range(0, count[i] + 1):
+                res += comb(odd, j) * comb(even, count[i] - j) * DFS(i - 1, odd - j, even - count[i] + j, balance - i * j)
+            return res % MOD
+        return 0 if total % 2 else DFS(9, n - n // 2, n // 2, total // 2)
