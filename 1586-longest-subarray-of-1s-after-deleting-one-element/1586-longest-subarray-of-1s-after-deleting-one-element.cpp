dddbe726aa75b738dcdac83b0235a63c126089d8
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-08-25 00:17:22 +0530

    Time: 66 ms (5.66%), Space: 76.6 MB (5.09%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,13 +1,44 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        const int n=nums.size();
-        int zeros=0, ans=0;
-        for(int l=0, r=0; r<n; r++){
-            zeros+=(nums[r]==0);
-            zeros-=(zeros>1 && nums[l++]==0);
-            ans=max(ans, r-l);
+        map<int, int> left, right;
+        int cnt = 0, ones = 0;
+
+        // Calculate left consecutive ones before each 0
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                cnt++;
+                ones++;
+            } else {
+                left[i] = cnt;
+                cnt = 0;
+            }
         }
-        return ans; 
+
+        // Edge cases
+        if (ones == 0) return 0;                     // No 1s
+        if (ones == nums.size()) return nums.size() - 1;  // All 1s
+
+        cnt = 0;
+
+        // Calculate right consecutive ones after each 0
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                right[i] = cnt;
+                cnt = 0;
+            }
+        }
+
+        // Calculate max combined length
+        int maxi = cnt;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                maxi = max(maxi, left[i] + right[i]);
+            }
+        }
+
+        return maxi;
     }
 };
