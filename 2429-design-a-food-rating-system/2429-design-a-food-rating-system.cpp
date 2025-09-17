425b0aa820f54a6b35193a596404175690bfc88a
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-09-18 01:05:41 +0530

    Time: 142 ms (59.28%), Space: 174.3 MB (21.05%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -1,26 +1,46 @@
 class FoodRatings {
-    unordered_map<string, string> foodToCuisine;
-    unordered_map<string, int> foodToRating;
-    unordered_map<string, set<pair<int, string>>> cuisineToFoods;
-
 public:
-    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        for (int i = 0; i < foods.size(); ++i) {
-            foodToCuisine[foods[i]] = cuisines[i];
-            foodToRating[foods[i]] = ratings[i];
-            cuisineToFoods[cuisines[i]].insert({-ratings[i], foods[i]});
+struct CustomPairCompare {
+    bool operator()(const pair<int,string>& a, const pair<int,string>& b) const {
+        if (a.first != b.first) {
+            return a.first > b.first; 
         }
+        return a.second < b.second;
+    }
+};
+    unordered_map<string,int> foodToRatings;
+    unordered_map<string,string> foodToCuisine;
+    unordered_map<string,set<pair<int,string>,CustomPairCompare>> cuisineToSet;
+
+
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int numberOfItems = foods.size();
+        for(int i=0;i<numberOfItems;i++){
+            foodToRatings[foods[i]] = ratings[i];
+            foodToCuisine[foods[i]] = cuisines[i];
+            cuisineToSet[cuisines[i]].insert({ratings[i],foods[i]});
+        }
+
     }
     
     void changeRating(string food, int newRating) {
-        string cuisine = foodToCuisine[food];
-        int oldRating = foodToRating[food];
-        cuisineToFoods[cuisine].erase({-oldRating, food});
-        cuisineToFoods[cuisine].insert({-newRating, food});
-        foodToRating[food] = newRating;
+        int oldRating  = foodToRatings[food];
+        foodToRatings[food] = newRating;
+        string Cuisine = foodToCuisine[food];
+        cuisineToSet[Cuisine].erase({oldRating,food});
+        cuisineToSet[Cuisine].insert({newRating,food});
     }
     
     string highestRated(string cuisine) {
-        return cuisineToFoods[cuisine].begin()->second;
+        auto it = next(cuisineToSet[cuisine].begin(),0);
+        pair<int,string> pr=*it;
+        return pr.second;
     }
 };
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
