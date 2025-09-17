4e98591caed7adaca098ac3557bf289465c63fd1
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-09-18 01:06:22 +0530

    Time: 140 ms (60.94%), Space: 163.4 MB (82.83%) - LeetHub

--- 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -1,41 +1,31 @@
 class FoodRatings {
+    using item=pair<int, string>;//(rating, food), (rating, cuisine)
+    unordered_map<string, set<item>> Rated;
+    unordered_map<string, item> mp;
 public:
-struct CustomPairCompare {
-    bool operator()(const pair<int,string>& a, const pair<int,string>& b) const {
-        if (a.first != b.first) {
-            return a.first > b.first; 
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings)
+    {
+        int n=foods.size();
+        for(int i=0; i<n; i++){
+            string food=foods[i], cuisine=cuisines[i];
+            int rating=ratings[i];
+            mp[food]={-rating, cuisine};//take minus
+            Rated[cuisine].insert({-rating, food});
         }
-        return a.second < b.second;
-    }
-};
-    unordered_map<string,int> foodToRatings;
-    unordered_map<string,string> foodToCuisine;
-    unordered_map<string,set<pair<int,string>,CustomPairCompare>> cuisineToSet;
-
-
-    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        int numberOfItems = foods.size();
-        for(int i=0;i<numberOfItems;i++){
-            foodToRatings[foods[i]] = ratings[i];
-            foodToCuisine[foods[i]] = cuisines[i];
-            cuisineToSet[cuisines[i]].insert({ratings[i],foods[i]});
-        }
-
     }
     
     void changeRating(string food, int newRating) {
-        int oldRating  = foodToRatings[food];
-        foodToRatings[food] = newRating;
-        string Cuisine = foodToCuisine[food];
-        cuisineToSet[Cuisine].erase({oldRating,food});
-        cuisineToSet[Cuisine].insert({newRating,food});
+        string& cuisine = mp[food].second;
+        int i = mp[food].first;
+        Rated[cuisine].erase({i, food});
+        Rated[cuisine].insert({-newRating, food});
+        mp[food]={-newRating, cuisine};
     }
     
     string highestRated(string cuisine) {
-        auto it = next(cuisineToSet[cuisine].begin(),0);
-        pair<int,string> pr=*it;
-        return pr.second;
+        return Rated[cuisine].begin()->second;
     }
+
 };
 
 /**
@@ -44,3 +34,10 @@ struct CustomPairCompare {
  * obj->changeRating(food,newRating);
  * string param_2 = obj->highestRated(cuisine);
  */
+ auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
