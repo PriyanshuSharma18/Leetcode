0ea19d5e9a322a85f1ef8fe15da13b375d5944af
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-09-27 20:20:18 +0530

    Time: 3 ms (35.26%), Space: 10.4 MB (86.95%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.cpp
+++ 0830-largest-triangle-area/0830-largest-triangle-area.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    double largestTriangleArea(vector<vector<int>>& points) {
-        const int n=points.size();
-        double maxArea=0;
-        for(int i=0; i<n-2; i++){
-            const int a0=points[i][0], a1=points[i][1];
-            for(int j=i+1; j<n-1; j++){
-                for(int k=j+1; k<n; k++){
-                    double area=0.5*((points[j][0]-a0)*(points[k][1]-a1)-(points[j][1]-a1)*(points[k][0]-a0));
-                    if (area<0) area=-area;
-                    maxArea=max(maxArea, area);
+    double largestTriangleArea(vector<vector<int>>&p) {
+        double res = 0;
 
+        int n = p.size();
+        for(int i=0; i<n-2; i++)
+            for(int j=i+1; j<n-1; j++)
+                for(int k=j+1; k<n; k++)
+                {
+                    double area = abs( (p[j][0]-p[i][0]) * (p[k][1]-p[i][1]) - (p[j][1] - p[i][1]) * (p[k][0] - p[i][0]) ) * 0.5;
+                    res = max(res, area);
                 }
-            }
-        }
-        return maxArea;
+        return res;
     }
 };
