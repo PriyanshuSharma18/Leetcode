22c0a24527fb7785a236029cd9b9ac9e32e798ec
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-10-29 00:08:41 +0530

    Time: 4 ms (68.35%), Space: 22.1 MB (67.89%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int leftSum = 0;
-        int rightSum = sum;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
-                    ans++;
+        
+        int total=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                int lsum=0;
+                for(int j=0;j<i;j++){
+                    lsum+=nums[j];
                 }
-                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
-                    ans++;
+                int rsum=0;
+                for(int j=i;j<nums.size();j++){
+                    rsum+=nums[j];
                 }
-            } else {
-                leftSum += nums[i];
-                rightSum -= nums[i];
+                if(lsum==rsum){
+                    total+=2;
+                }else if((lsum==(rsum-1)) || (rsum==(lsum-1))){
+                    total++;
+                }
             }
         }
-        return ans;
+        return total;
     }
 };
