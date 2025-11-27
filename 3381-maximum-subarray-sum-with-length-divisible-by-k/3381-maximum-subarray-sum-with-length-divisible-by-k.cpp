e8224c32c18dcda4130b7dac6aa349aa7adcc4ac
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2025-11-28 00:57:26 +0530

    Time: 17 ms (46.42%), Space: 162.2 MB (98.63%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,16 +1,24 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
 class Solution {
 public:
-    using ll=long long;
-    static long long maxSubarraySum(vector<int>& nums, int k) {
-        const int n=nums.size();
-        vector<ll> minS(k, LLONG_MAX/2);
-        ll prefix=0, ans=LLONG_MIN;
-        minS[k-1]=0;
-        for(int i=0; i<n; i++){
-            prefix+=nums[i];
-            ll& ksum=minS[i%k];
-            ans=max(ans, prefix-ksum);
-            ksum=min(prefix, ksum);  
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long prefix = 0;
+        const long long INF = (long long)1e18;
+        vector<long long> min_pre(k, INF);
+        min_pre[0] = 0;
+        long long ans = -INF;
+        for (int j = 1; j <= n; ++j) {
+            prefix += nums[j-1];
+            int r = j % k;
+            if (min_pre[r] != INF) {
+                long long cand = prefix - min_pre[r];
+                if (cand > ans) ans = cand;
+            }
+            if (prefix < min_pre[r]) min_pre[r] = prefix;
         }
         return ans;
     }
