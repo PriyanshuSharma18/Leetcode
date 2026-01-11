19cdd91cd22dc360a0aa2264582428dcf96c64f8
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2026-01-12 00:06:52 +0530

    Time: 11 ms (33.61%), Space: 17.9 MB (69.66%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        stack<int> st;
-        heights.push_back(0);  // sentinel
-        int maxArea = 0;
-
-        for (int i = 0; i < heights.size(); i++) {
-            while (!st.empty() && heights[i] < heights[st.top()]) {
-                int h = heights[st.top()];
-                st.pop();
-
-                int width = st.empty() ? i : i - st.top() - 1;
-                maxArea = max(maxArea, h * width);
-            }
-            st.push(i);
-        }
-
-        heights.pop_back(); // clean up
-        return maxArea;
-    }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty()) return 0;
+        if(matrix.empty() || matrix[0].empty()) return 0;
 
         int rows = matrix.size();
         int cols = matrix[0].size();
-        vector<int> heights(cols, 0);
 
+        vector<int> heights(cols, 0);
         int maxArea = 0;
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (matrix[r][c] == '1')
-                    heights[c]++;
-                else
-                    heights[c] = 0;
+        for(int i = 0; i < rows; ++i){
+            for(int j = 0; j < cols; ++j){
+                heights[j] = (matrix[i][j] == '1') ? heights[j] + 1 : 0;
             }
             maxArea = max(maxArea, largestRectangleArea(heights));
-        }
 
+        }
+        return maxArea;
+    }
+private: 
+    int largestRectangleArea(vector<int>& heights){
+        // with single pass monotonic stack
+        stack<int> st;
+        int maxArea = 0;
+        int n = heights.size();
+
+        for(int i = 0; i <= n; ++i){
+            int h = (i == n) ? 0 : heights[i]; // 0 is sentinel value
+            while(!st.empty() && h < heights[st.top()]){
+                int curHeight = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i - st.top() - 1;
+                maxArea = max(maxArea, curHeight * width);
+            }
+            st.push(i);
+        }
         return maxArea;
     }
 };
