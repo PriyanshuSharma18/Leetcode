79d9a48c578a3f47e9ef87208e3a228e244b5323
PriyanshuSharma18 <113844643+PriyanshuSharma18@users.noreply.github.com>
2026-04-02 23:56:50 +0530

    Time: 277 ms (74.47%), Space: 109 MB (80.43%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,19 +1,27 @@
+int dp[500][500][3]; // DP array 
 class Solution {
-public:
-    int maximumAmount(vector<vector<int>>& coins) {
-        int n = coins.size(), m = coins[0].size();
-        vector dp(n, vector(m, vector<int>(3, -1e9)));
-        dp[0][0][1] = dp[0][0][2] = 0, dp[0][0][0] = coins[0][0];
+    static int f(int i, int j, int k, vector<vector<int>>& coins) {
+        if (i==0 && j==0) {// // Base case 
+            if (k>0) return max(0, coins[0][0]); 
+            return coins[0][0]; 
+        }
+        if (i<0 || j<0 || k<0) return -1e9;
+
+        if (dp[i][j][k] != INT_MIN) return dp[i][j][k];
+        const int x=coins[i][j];
+        int ans=x+max(f(i-1, j, k, coins), f(i, j-1, k, coins));
+        // If x<0, k>0 neutralize robber once
+        if (x<0 && k>0) 
+            ans=max({ans, f(i-1, j, k-1, coins), f(i, j-1, k-1, coins)});
         
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                for (int k = 0; k < 3; k++) {
-                    if (i) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j]);
-                    if (i && k) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k - 1]);
-                    if (j) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j]);
-                    if (j && k) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k - 1]);
-                }
-        int ans = *max_element(dp[n - 1][m - 1].begin(), dp[n - 1][m - 1].end());
-        return ans;
+        return dp[i][j][k]=ans;
+    }
+
+public:
+    static int maximumAmount(vector<vector<int>>& coins) {
+        const int r=coins.size(), c=coins[0].size();
+        // Initialize DP with INT_MIN
+        fill(&dp[0][0][0], &dp[0][0][0]+500*500*3, INT_MIN);
+        return f(r-1, c-1, 2, coins);
     }
 };
